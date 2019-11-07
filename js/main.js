0fd466423ac0928f33879db11bbe7e33ddb64686
SOUNAK <sounakume@gmail.com>
2019-11-07 16:51:42 +0530

    Functionality adjustments

--- js/main.js
+++ js/main.js
@@ -9,20 +9,28 @@ function getMovies() {
 
                 let movies = data.Search;
                 let output = '';
-                movies.map(movie => {
-                    output += `
-                    <div class="card">
-                      <img src="${movie.Poster}">
-                      <div class="container">
-                         <h5><b>${movie.Title}</b></h5>
-                         <p>
-                           <a onclick="movieSelected('${movie.imdbID}')" class="" href="#">Movie Details</a>
-                         </p>
-                      </div>
-                    </div>
-                    
-                `;
-                })
+
+                if (typeof movies !== 'undefined' && movies.length > 0) {
+                    movies.map(movie => {
+                        output += `
+                        <div class="card">
+                          <img src="${movie.Poster}">
+                          <div class="container">
+                             <h5><b>${movie.Title}</b></h5>
+                             <p>
+                               <a onclick="movieSelected('${movie.imdbID}')" class="" href="#">Movie Details</a>
+                             </p>
+                          </div>
+                        </div>
+                        
+                    `;
+                    })
+                }
+
+
+                if (output == '') {
+                    output = `<div><h1 class="no-results">Sorry! No results found</h1></div>`
+                }
 
 
                 var d1 = document.querySelector('#extra');
